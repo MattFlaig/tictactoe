9fedddd733bb9f3d99b80f0ed8c0f4bdd7f57485
matthias_laptop <matt.flaig@web.de>
2015-10-02 11:07:49 +0200

    balanced mode

--- js/balanced/computer_balanced.js
+++ js/balanced/computer_balanced.js
@@ -1,7 +1,6 @@
 //COMPUTER PLAYER OBJECT (BALANCED MODE)
 var computer = {
   choices : [],
-  results : [],
   colour : "orange",
   possibleChoices : [],
 
@@ -9,12 +8,8 @@ var computer = {
     computer.possibleChoices = computer.makeUnique(computer.possibleChoices);
     for(var i=0; i<chosen.length;++i){
       var choice = chosen[i];
-      for (var j=0;j<computer.possibleChoices.length;++j){
-        var possible = computer.possibleChoices[j];
-        if(possible == choice){
-          computer.possibleChoices.splice(j,1);
-        }
-      }
+      var index = computer.possibleChoices.indexOf(choice);
+      if(index >= 0){ computer.possibleChoices.splice(index,1); }
     }
   },
   makeUnique : function(array){
@@ -28,10 +23,12 @@ var computer = {
     return uniqueArray;
   },
   balancedStrategy : function(){
+    alert("before: " + computer.possibleChoices);
     computer.prepareChoices();
     var nextChoice = computer.searchForWin();
     if (nextChoice == "noSuccess"){
       nextChoice = computer.findBestMove();
+      alert("after: " + computer.possibleChoices);
     }
     return nextChoice;
   },
@@ -45,14 +42,12 @@ var computer = {
   findAlternativeChoices : function(){
     if(computer.possibleChoices.length > 0){
       var nextChoice = computer.getNextChoice(computer.possibleChoices);
-      if(nextChoice == "stillNoAlternative"){nextChoice = computer.getNextChoice(board.fields);}
     }
-    else{nextChoice = computer.getNextChoice(board.fields);}
+    else{ nextChoice = computer.getNextChoice(board.fields); }
     return nextChoice;
   },
   getNextChoice : function(array){
-    var nextChoice = computer.getRandomChoice(array);
-    return (parseInt(nextChoice) > 0) ? nextChoice : "stillNoAlternative";
+    return computer.getRandomChoice(array);
   },
   getRandomChoice : function(array){
     var possibleIndex = Math.floor(Math.random() * (array.length));
@@ -65,81 +60,48 @@ var computer = {
   },
   findEgocentricChoices : function(){
     for(var i=0;i<board.fields.length;++i){
-    var firstPossible = board.fields[i];
       for(var j=0;j<board.fields.length;++j){
-        var secondPossible = board.fields[j];
-        if(secondPossible != firstPossible){
+        var firstPossible = board.fields[i], secondPossible = board.fields[j];
+        if(secondPossible !== firstPossible){
           computer.manageChoices(firstPossible,secondPossible);
         }
       }
     }
   },
-  manageChoices : function(firstPossible,secondPossible){
-    var possibleAddedFields = parseInt(firstPossible) + parseInt(secondPossible);
+  manageChoices : function(first, second){
     var adder = parseInt(computer.getAdder());
-    if(adder > 0 && possibleAddedFields == adder){
-      computer.possibleChoices.push(firstPossible, secondPossible);
+    if(adder > 0 && parseInt(first) + parseInt(second) === adder){
+      computer.possibleChoices.push(first, second);
       computer.possibleChoices = computer.makeUnique(computer.possibleChoices);
     }
   },
   getAdder : function(){
-    if (ticTacToe.round<=3){var adderIndex=0;}
-    else {var adderIndex = Math.floor(ticTacToe.round/3);}
-    var adder = ticTacToe.WINNING_SUM - parseInt(computer.choices[adderIndex]);
-    return adder;
+    var ai;
+    ticTacToe.round<=3 ? ai = 0 : ai = Math.floor(ticTacToe.round/3);
+    return ticTacToe.WINNING_SUM - parseInt(computer.choices[ai]);
   },
+
   searchForWin : function(){
-    var noSuccess = "noSuccess";
-    var nextChoice = computer.getWinningMove(noSuccess);
-    if(nextChoice){return nextChoice;}
-    else{return noSuccess;}
+    var nextChoice = computer.getWinningMove();
+    return nextChoice ? nextChoice : "noSuccess";
   },
-  getWinningMove : function(noSuccess){
+  getWinningMove : function(){
     for(var i = 0;i < board.fields.length; ++i){
-      var possibleWin = parseInt(board.fields[i]);
-      if(ticTacToe.round == 5 || ticTacToe.round == 6){
-        var nextChoice = computer.winInNextRounds(possibleWin);
-        if (nextChoice == possibleWin){break;}
-      }
-      else if(ticTacToe.round < 5){
-        var addedChoices = parseInt(computer.choices[0]) + parseInt(computer.choices[1]);
-        if(possibleWin + addedChoices == ticTacToe.WINNING_SUM){var nextChoice = possibleWin;break;}
-        else{var nextChoice = noSuccess;}
-      }
-      else{
-        var nextChoice = computer.winInLastRounds(possibleWin);
-        if (nextChoice == possibleWin){break;}
-      }
+      var nextChoice = computer.winInNextRounds(board.fields[i]);
+      if (nextChoice === board.fields[i]){ break; }
     }
     return nextChoice;
   },
   winInNextRounds : function(possibleWin){
-    for(var j = 0;j < computer.results.length; ++j){
-      var addedChoices = parseInt(computer.results[j]);
-      if(possibleWin + addedChoices == ticTacToe.WINNING_SUM){
-        var nextChoice = possibleWin;break;
-      }
-    }
-    var stillNoSuccess = "noSuccess";
-    if(nextChoice == possibleWin){return nextChoice;}
-    else {return stillNoSuccess;}
-  },
-  winInLastRounds : function(possibleWin){
+    var winner = false;
     for(var k = 0;k < computer.choices.length; ++k){
-      var firstChoice = computer.choices[k];
       for(var l = k + 1;l < computer.choices.length; ++l){
-        var secondChoice = computer.choices[l];
-        var combined = parseInt(firstChoice) + parseInt(secondChoice);
-        var combinedChoice = possibleWin + combined;
-        if(combinedChoice === ticTacToe.WINNING_SUM){
-          var nextChoice = possibleWin;break;
-        }
+        if((parseInt(parseInt(computer.choices[k])
+          + parseInt(computer.choices[l]))
+          + parseInt(possibleWin)) === ticTacToe.WINNING_SUM){ winner = true; break; }
       }
     }
-    var stillNoSuccess = "noSuccess";
-    if(nextChoice == possibleWin){return nextChoice;}
-    else {return stillNoSuccess;}
+    return winner ? possibleWin : "noSuccess";
   }
-
 
 }
